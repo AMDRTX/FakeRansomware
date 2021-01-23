1aa2f55ae6309dea2f9b3505c1ad9eb3d0662ddc
validator <73761107+AMDRTX@users.noreply.github.com>
2021-01-23 15:01:30 +0000

    Add files via upload
    FakeRansomware 1.1

---
+++ Fake ransomware/Prank.py
@@ -0,0 +1,21 @@
+import time
+import os
+print ("FILL IN THE QUESTION BELOW")
+Name = input ("What is your name:")
+print ("SAY GOODBYE")
+print (Name)
+time.sleep(5)
+with open ('PeopletoKILL.txt' , 'w') as f:
+    f.write(Name)
+    with open ('README.txt' , 'w') as f:
+        f.write("Your computer has been infected with Slacer Ransomware pay me 1000$  on Paypal -")
+        f.write(Name)
+        f.write("@gmail.com")
+        time.sleep(5)
+with open ('Note.txt' , 'w') as f:
+    f.write("No your computer doesn't have a virus it's just a prank coded by Hackamous on YT")
+    print ("OPEN THE PeopletoKILL.txt file")
+with open ('666.txt' , 'w') as f:
+   f.write("Aw wouldn't it be nice if you DIED")
+print ("Now open the README.txt file")
+print ("now open the important.txt file")
